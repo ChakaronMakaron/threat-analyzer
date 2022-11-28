3d495be0c2d37a44aed064a9e01e508415338695
le makhno <voroshilov-klim@ukr.net>
2022-11-28 17:38:54 +0200

    init

---
+++ src/main/java/com/lemakhno/threatanalyzer/ThreatAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.lemakhno.threatanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreatAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreatAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lemakhno/threatanalyzer/ThreatAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemakhno.threatanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreatAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
